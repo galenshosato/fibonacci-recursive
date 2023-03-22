5704e59f927d7cf2c1e4ff3286c7cfd31d11421b
Galen Sho Sato <galen.sato@gmail.com>
2023-03-22 17:53:15 -0400

    completed lab

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,15 @@
 function fibonacci(n) {
-  // type your code here
+  if (n === 0) {
+    return n
+  }
+
+  if (n === 1) {
+    return n
+  }
+
+  return fibonacci(n-1) + fibonacci(n-2)
+
+  
 }
 
 if (require.main === module) {
